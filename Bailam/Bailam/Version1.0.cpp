1f6f704d983f305105a5e4e099eec981b2a52ed6
Administrator <Administrator@P5_19>
2020-10-17 16:12:01 +0700

    Version1

---
+++ Bailam/Bailam/Version1.0.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void main()
+{
+	int a, b;
+	cout << "nhap so a :";
+	cin >> a;
+	cout << "nhap so b :";
+	cin >> b;
+	cout << "phep cong : " << a + b;
+	cout << "\nphep tru :" << a - b;
+	cout << "\nphep nhan :" << a * b;
+	cout << "\nphep chia : " << a / b;
+}
